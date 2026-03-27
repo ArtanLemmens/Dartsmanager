9a0552cff3002a45d94c18891b07e4e640b77037
Artan Lemmens <artlem@vdh.local>
2026-03-27 13:19:52 +0100

    toevoegen TorunamentService CRUD (inclu Status CRUD, verwijderen StatusService)

---
+++ Services/TournamentService.cs
@@ -0,0 +1,146 @@
+﻿using Dartsmanager.Data;
+using Dartsmanager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dartsmanager.Services
+{
+    public class TournamentService
+    {
+        // Tornooi
+        public static List<Tournament> GetAll()
+        {
+            using (var db = new DbDartsmanagerContext())
+            {
+                var tornooien = db.Tournaments.Include(t => t.Adres).Include(t => t.Status).ToList();
+                return tornooien;
+            }
+        }
+        public static void Update(Tournament tornooi)
+        {
+            try
+            {
+                using (var db = new DbDartsmanagerContext())
+                {
+                    var bestaandTornooi = db.Tournaments.FirstOrDefault(t => t.Id == tornooi.Id);
+                    if (bestaandTornooi != null)
+                    {
+                        bestaandTornooi.Naam = tornooi.Naam;
+                        bestaandTornooi.AdresId = tornooi.AdresId;
+                        bestaandTornooi.Jaargang = tornooi.Jaargang;
+                        bestaandTornooi.Datum = tornooi.Datum;
+                        bestaandTornooi.StatusId = tornooi.StatusId;
+                        bestaandTornooi.MaxInschrijvingen = tornooi.MaxInschrijvingen;
+                        bestaandTornooi.AantalRondes = tornooi.AantalRondes;
+                        bestaandTornooi.ActieveRonde = tornooi.ActieveRonde;
+                        bestaandTornooi.ExtraInfo = tornooi.ExtraInfo;
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("De wijzigingen konden niet worden opgeslagen. Controleer of het tornooi nog wordt gebruikt in andere gegevens.");
+            }
+        }
+        public static void Add(Tournament tornooi)
+        {
+            try
+            {
+                using (var db = new DbDartsmanagerContext())
+                {
+                    db.Tournaments.Add(tornooi);
+                    db.SaveChanges();
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("Kon het tornooi niet toevoegen. Controleer of de naam uniek is of de databaseverbinding juist is.");
+            }
+        }
+        public static void Remove(Tournament tornooi)
+        {
+            try
+            {
+                using (var db = new DbDartsmanagerContext())
+                {
+                    var bestaandTornooi = db.Tournaments.FirstOrDefault(t => t.Id == tornooi.Id);
+                    if (bestaandTornooi != null)
+                    {
+                        db.Tournaments.Remove(bestaandTornooi);
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("Dit tornooi kan niet worden verwijderd omdat hij nog gekoppeld is aan een project of andere gegevens.");
+            }
+        }
+
+        // Status
+        public static List<Status> GetAllStatus()
+        {
+            using (var db = new DbDartsmanagerContext())
+            {
+                var statuses = db.Statuses.ToList();
+                return statuses;
+            }
+        }
+        public static void UpdateStatus(Status status)
+        {
+            try
+            {
+                using (var db = new DbDartsmanagerContext())
+                {
+                    var bestaandeStatus = db.Statuses.FirstOrDefault(s => s.Id == status.Id);
+                    if (bestaandeStatus != null)
+                    {
+                        bestaandeStatus.Naam = status.Naam;
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("De wijzigingen konden niet worden opgeslagen. Controleer of de status nog wordt gebruikt in andere gegevens.");
+            }
+        }
+        public static void AddStatus(Status status)
+        {
+            try
+            {
+                using (var db = new DbDartsmanagerContext())
+                {
+                    db.Statuses.Add(status);
+                    db.SaveChanges();
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("Kon de status niet toevoegen. Controleer of de naam uniek is of de databaseverbinding juist is.");
+            }
+        }
+        public static void RemoveStatus(Status status)
+        {
+            try
+            {
+                using (var db = new DbDartsmanagerContext())
+                {
+                    var bestaandeStatus = db.Statuses.FirstOrDefault(s => s.Id == status.Id);
+                    if (bestaandeStatus != null)
+                    {
+                        db.Statuses.Remove(bestaandeStatus);
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("Deze status kan niet worden verwijderd omdat hij nog gekoppeld is aan een project of andere gegevens.");
+            }
+        }
+    }
+}
